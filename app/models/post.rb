8d8b28778629e6c870473ff52d5f88daa2928b17
adames <adameshh@gmail.com>
2017-08-21 11:29:39 -0400

    updated app

---
+++ app/models/post.rb
@@ -0,0 +1,19 @@
+require 'redd'
+
+class Post < ApplicationRecord
+
+  def call_api
+    session = Redd.it(
+    user_agent: 'BetterReddit',
+    client_id:  'tSojb2-za9wAgw',
+    secret:     'R41xQZ37zTJXYfNQdHQhD8FMBSo',
+    username:   'Better_Reddit_API',
+    password:   'orange10'
+    )
+
+    session.subreddit('all').comment_stream do |comment|
+      print comment
+    end
+  end
+  
+end
